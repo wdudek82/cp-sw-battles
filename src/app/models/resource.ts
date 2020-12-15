32a3bbc251f58e6c814fcfd38e657cf7aae78ccd
Wojciech Dudek <wdudek82@gmail.com>
2020-12-16 00:02:48 +0100

    Update model

--- src/app/models/resource.ts
+++ src/app/models/resource.ts
@@ -1,7 +1,7 @@
 export interface Resource {
   name: string;
-  url: string;
-  films: string[];
-  created: Date;
-  edited: Date;
+  url?: string;
+  films?: string[];
+  created?: Date;
+  edited?: Date;
 }
